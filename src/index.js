f331f707b67688b3225eec93a9e52c12316bd180
Daniel Garcia <jdanielink@gmail.com>
2020-01-08 00:04:47 -0600

    Fixed cors problem with react

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const options = {
   port: process.env.PORT || 4000,
   cors: {
     credentials: true,
-    origin: [process.env.FRONTEND_ENDPOINT]
+    origin: [process.env.FRONTEND_ENDPOINT, 'http://localhost:3005']
   }
 };
 
